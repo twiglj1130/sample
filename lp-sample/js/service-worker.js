d6a731989ae4f3c3c77af79d45f3aa522883ad3d
twiglj1130 <twig_lj_1130@yahoo.co.jp>
2024-09-07 15:20:26 +0900

    Add files via upload

---
+++ lp-sample/js/service-worker.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'my-site-cache-v1';
+const urlsToCache = [
+    '/', // ホームページのルート
+    '/plus-feel_lp.html', // プラスフィールLPのHTMLファイル
+    '/non-critical.css', // LPのCSSファイル
+    '/js/plus-feel.js', // メインのJavaScriptファイル
+    // その他キャッシュしたいリソースをここに追加
+];
+
+// インストールイベント
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// フェッチイベント
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
+
+// アクティベートイベントで古いキャッシュを削除
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
